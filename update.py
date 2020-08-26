1d3177052990bce96759c7fd674a46d89219904e
Matt Mielke <matt.mielke@digikey.com>
2020-08-26 09:26:21 +0000

    Refactor device scanning code
    Created the ScannerUI class to manage the scanning user interface
    (displaying and updating the table of devices and recieving user input)
    Also, deleted the ScanDelegate class as I couldn't find a use for it.
    Simply using the DefaultDelegate now.

--- update.py
+++ update.py
@@ -17,80 +17,155 @@ class Delegate(btle.DefaultDelegate):
         self.data = 0
 
     def handleNotification(self, cHandle, data):
+        print(f"Notification from {hex(cHandle)}: {data}")
         self.handle = cHandle
         self.data = data
 
 
-class ScanDelegate(btle.DefaultDelegate):
+class ScannerUI():
+    _choiceWidth = 6
     _nameWidth = 16
+    _macAddrWidth = 17
+    _rssiWidth = 7
 
     def __init__(self):
-        super().__init__()
-        self.devDict = {}
+        self.userInput = queue.Queue()
+        self.reset()
+
+    def reset(self):
+        """Reset the UI. 
+        
+        For example, if the selected device failed to connect and the user must
+        choose again.
+        """
         self.devCount = 0
-        print("Scanning for devices...")
-        print()
-        print(f" Choice | {'Device Name':^{self._nameWidth}} |    MAC Address    |  RSSI ")
-        print('-' * (41 + self._nameWidth))
-        print()
-        print("Choose a device to update: ", end='')
+        self._userSelection = None
+        self._errMsg = ''
 
-    def handleDiscovery(self, scanEntry, isNewDev, isNewData):
-        # Add to list
-        if isNewDev:
-            self.devDict[scanEntry.addr] = scanEntry
-            self.devCount = self.devCount + 1
-            self._printDevInfo(scanEntry)
-        # Update list TODO Do I really need this?
-        elif isNewData:
-            self.devDict[scanEntry.addr] = scanEntry
+    def update(self, devices):
+        """Update the UI by including the supplied devices in the table"""
+        if devices == []:
+            return
 
-    def _printDevInfo(self, dev):
-        devName = dev.getValueText(9)
+        # If these are the first devices being displayed
+        if self.devCount == 0:
+            self._displayHeader()
+            # For now, we only want input when there are devices to choose from
+            threading.Thread(target=self._getUserInput).start()
+
+        # Remove the prompt and move cursor to just below table
+        self._moveCursorLeft(999) # Move cursor to beginning of line
+        self._clearCursorToEnd() # Clear the line
+        self._moveCursorUp()
+
+        for device in devices:
+            self.devCount += 1
+
+            # Display the new device in the table
+            self._displayDevice(device)
+
+        # Re-display the prompt
+        print()
+        self._displayPrompt()
+
+    @property
+    def userSelection(self):
+        """Get and cache the user's selection."""
+        if self._userSelection == None: # If the user has not yet made a valid selection
+            if not self.userInput.empty(): # If the user has provided some input
+                selection = self.userInput.get() # Remove from queue
+                
+                # Validate the user's input
+                try:
+                    selection = int(selection)
+                except:
+                    self._errMsg = f"Choice \"{selection}\" not valid. "
+                else:
+                    if (selection < 1) or (selection > self.devCount):
+                        self._errMsg = f"Device {selection} is not on the list. "
+                    else:
+                        self._userSelection = selection
+                        return self._userSelection
+                
+                # Re-print the prompt (now with appropriate error message)
+                self._moveCursorUp()
+                self._moveCursorLeft(999)
+                self._clearCursorToEnd()
+                self._displayPrompt()
+                threading.Thread(target=self._getUserInput).start()
+
+        return self._userSelection 
+
+    def _displayHeader(self):
+        """Display the table header."""
+        print(
+            f" {'Choice':^{self._choiceWidth}} |"
+            f" {'Device Name':^{self._nameWidth}} |"
+            f" {'MAC ADDRESS':^{self._macAddrWidth}} |"
+            f" {'RSSI':^{self._rssiWidth}} "
+        )
+        print('-' * (2 + self._choiceWidth)
+            + '+' + '-' * (2 + self._nameWidth)
+            + '+' + '-' * (2 + self._macAddrWidth)
+            + '+' + '-' * (2 + self._rssiWidth)
+        )
+        print() 
+
+    def _displayDevice(self, device):
+        """Display device the info as a table row."""
+        devName = device.getValueText(9)
         if devName == None:
             devName = "<No Name>"
 
-        print("\x1B[99D\x1B[K\x1B[1A", end='')
-        print(f"{self.devCount:^8}| {devName[:self._nameWidth]:<{self._nameWidth}} | {dev.addr} | {f'{dev.rssi} dB':^7} ")
-        print()
+        print(
+            f" {self.devCount:^{self._choiceWidth}} |"
+            f" {devName[:self._nameWidth]:<{self._nameWidth}} |"
+            f" {device.addr:^{self._macAddrWidth}} |"
+            f" {f'{device.rssi} dB':^{self._rssiWidth}} "
+        )
+
+    def _displayPrompt(self):
+        print(self._errMsg, end='')
         print("Choose a device to update: ", end='', flush=True)
 
-q = queue.Queue()
+    def _getUserInput(self):
+        self.userInput.put(input())
 
-def getSelection():
-    q.put(input())
+    def _moveCursorLeft(self, count=1):
+        print(f"\x1B[{count}D", end='')
+
+    def _moveCursorUp(self, count=1):
+        print(f"\x1B[{count}A", end='')
+
+    def _clearCursorToEnd(self):
+        print("\x1B[K", end='')
 
 
 if __name__ == '__main__':
     # Create a scanner object that sends BLE broadcast packets to the ScanDelegate
-    scanner = btle.Scanner().withDelegate(ScanDelegate())
+    scanner = btle.Scanner()
+
+    # Create an object to manage the scanner user interface
+    scannerUI = ScannerUI()
 
     # Start the scanner
     scanner.start()
+    print("Scanning for devices...")
+    print()
 
-    while True:
-        # Create and start a thread to get the user's device selection
-        threading.Thread(target=getSelection).start()
+    # Continuously scan for devices while waiting for the user to choose one
+    while scannerUI.userSelection == None:
+        scannerUI.update(list(scanner.getDevices())[scannerUI.devCount:])
+        scanner.process(1)
 
-        # Continuously scan for devices until the user makes a selection
-        while q.empty():
-            scanner.process(1)
+    # Stop the scanner
+    try:
+        scanner.stop()
+    except:
+        print("Error stopping scanner.")
 
-        choice = q.get()
-        try:
-            choice = int(choice)
-        except:
-            print(f"\x1B[99D\x1B[K\x1B[1AChoice \"{choice}\" not valid. Choose a device to update: ", end='', flush=True)
-            continue
-
-        # Validate the users selection
-        if (choice < 1) or (choice > scanner.delegate.devCount):
-            print(f"\x1B[99D\x1B[K\x1B[1ADevice {choice} is not on the list. Choose a device to update: ", end='', flush=True)
-        else:
-            device = list(scanner.delegate.devDict.values())[choice-1]
-            break
-
-    scanner.stop()
+    # Retrieve the selected device
+    device = list(scanner.getDevices())[scannerUI.userSelection-1]
 
     # Connect to the user selected device
     try:
@@ -145,6 +220,7 @@ if __name__ == '__main__':
     # Send the Exit DFU command
     hostCmd.exitDFU()
 
+    # TODO Make this better...
     try:
         target.disconnect()
     except:
